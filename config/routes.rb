a7c924d2047e2bda4523a3c9b82ae144d1cc6d68
rufatweb <rufat.web@gmail.com>
2019-03-11 13:54:18 -0400

    add auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      get '/current_user', to: "auth#show"
     end
   end
 end
