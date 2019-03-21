0fa9f433a819c1284a8703fcaf39ffe0f30f7b24
rufatweb <rufat.web@gmail.com>
2019-03-21 11:59:10 -0400

    add imgUrl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_170008) do
+ActiveRecord::Schema.define(version: 2019_03_21_151221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_170008) do
     t.string "email"
     t.string "title"
     t.string "location"
+    t.string "imgUrl"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,42 +12,48 @@ User.create!([{
   email: "david@gmail.com",
   title: "photographer",
   password: "123",
-  location: "New York City, NY"
+  location: "New York City, NY",
+  imgUrl: "https://aoclinics.com/wp-content/uploads/2016/03/lhr-men-fb.jpg"
 },
 {
   username: "Rufat",
   email: "rufat@gmail.com",
   title: "photographer",
   password: "123",
-  location: "Los Angeles, CA"
+  location: "Los Angeles, CA",
+  imgUrl: "https://oldnavy.gap.com/webcontent/0011/247/651/cn11247651.jpg"
 },
 {
   username: "Amy",
   email: "amy@gmail.com",
   title: "photographer",
   password: "123",
-  location: "New York City, NY"
+  location: "New York City, NY",
+  imgUrl: "https://yt3.ggpht.com/a-/AAuE7mCwIA_06EHOqyDXtYH6SJXvuMRKw2YVlS8kgQ=s900-mo-c-c0xffffffff-rj-k-no"
 },
 {
   username: "Sarah",
   email: "sarah@gmail.com",
   title: "influencer",
   password: "123",
-  location: "Los Angeles, CA"
+  location: "Los Angeles, CA",
+  imgUrl: "https://2.gravatar.com/avatar/edba75642767639a36e8d340ae89d40d?s=400&d=mm&r=g"
 },
 {
   username: "Kevin",
   email: "kevin@gmail.com",
   title: "influencer",
   password: "123",
-  location: "New York City, NY"
+  location: "New York City, NY",
+  imgUrl: "https://images-na.ssl-images-amazon.com/images/I/814zJjE%2B4yL._UX385_.jpg"
 },
 {
   username: "Adam",
   email: "adam@gmail.com",
   title: "influencer",
   password: "123",
-  location: "Los Angeles, CA"
+  location: "Los Angeles, CA",
+  imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsot6AFVUcu8fACC12wxqWv44UcDHuyYdZHaaTjgZi2Sx7U4_y"
 }])
 
 p "Created #{User.count} users"
